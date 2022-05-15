bb701f8eed1ee7f8cf967abcbb297ee3af52ba5b
Hye Ryun Cho <absolutecool18@gmail.com>
2022-05-15 21:53:22 +0900

    <feat>flutter layout study

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('앱임')
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
         ),
-        body: Text(('안녕')
-        ),
-        bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(Icons.phone),
-            Icon(Icons.message),
-            Icon(Icons.contact_page_outlined)
-          ],
-        ),
-      ))
-    );
+        home: Scaffold(
+            appBar: AppBar(title: Text('Study Flutter'), actions: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.search),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.menu),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.notifications),
+              )
+            ]),
+            body: SizedBox(
+                child: Row(children: [
+              Image.network(
+                'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201706/23/b71449f8-e830-45a0-bb4d-7b1a328e19f2.jpg',
+                width: 300,
+                height: 150,
+              ),
+              Row(
+                children: [
+                  Column(children: [
+                    Text('Title',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 30)),
+                    Text('@location'),
+                    Text('price'),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [Icon(Icons.favorite_outline), Text('hits')])
+                  ]),
+                ],
+              )
+            ]))));
   }
 }

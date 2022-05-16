0fe460e4a6ac47c6537787fccfbc07daf0bcb3ae
Hye Ryun Cho <absolutecool18@gmail.com>
2022-05-17 00:02:28 +0900

    <feat>layout exercise

--- lib/main.dart
+++ lib/main.dart
@@ -11,46 +11,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blueGrey,
-        ),
-        home: Scaffold(
-            appBar: AppBar(title: Text('Study Flutter'), actions: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(Icons.search),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(Icons.menu),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(Icons.notifications),
-              )
-            ]),
-            body: SizedBox(
-                child: Row(children: [
-              Image.network(
-                'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201706/23/b71449f8-e830-45a0-bb4d-7b1a328e19f2.jpg',
-                width: 300,
-                height: 150,
-              ),
-              Row(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: Scaffold(
+          appBar: AppBar(title: Text('Study Flutter'), actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.search),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.menu),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.notifications),
+            )
+          ]),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(children: [
+              Column(
                 children: [
-                  Column(children: [
-                    Text('Title',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 30)),
-                    Text('@location'),
-                    Text('price'),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [Icon(Icons.favorite_outline), Text('hits')])
-                  ]),
+                  Image.network(
+                    'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201706/23/b71449f8-e830-45a0-bb4d-7b1a328e19f2.jpg',
+                    width: 300,
+                    height: 150,
+                  ),
                 ],
-              )
-            ]))));
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(children: [
+                  Text('Title',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+                  Text('@location'),
+                  Text('price'),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [Icon(Icons.favorite_outline), Text('hits')])
+                ]),
+              ),
+            ]),
+          )),
+    );
   }
 }

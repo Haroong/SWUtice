562088741d11b590356818449742dc619c26f788
Hye Ryun Cho <absolutecool18@gmail.com>
2022-05-19 22:53:54 +0900

    <feat>layout exercise alignment

--- lib/main.dart
+++ lib/main.dart
@@ -32,30 +32,33 @@ class MyApp extends StatelessWidget {
           ]),
           body: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Row(children: [
-              Column(
-                children: [
-                  Image.network(
-                    'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201706/23/b71449f8-e830-45a0-bb4d-7b1a328e19f2.jpg',
-                    width: 300,
-                    height: 150,
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(children: [
-                  Text('Title',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
-                  Text('@location'),
-                  Text('price'),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [Icon(Icons.favorite_outline), Text('hits')])
-                ]),
-              ),
-            ]),
+            child: Container(
+              height: 150,
+              child: Row(children: [
+                Image.network(
+                  'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201706/23/b71449f8-e830-45a0-bb4d-7b1a328e19f2.jpg',
+                  width: 300,
+                  height: 150,
+                ),
+                Container(
+                  width: 300,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Flexible(
+                            child: Text('Title',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 30)),
+                            flex: 5),
+                        Flexible(child: Text('@location'), flex: 5),
+                        Flexible(child: Text('price'), flex: 5),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [Icon(Icons.favorite), Text('100')])
+                      ]),
+                ),
+              ]),
+            ),
           )),
     );
   }
